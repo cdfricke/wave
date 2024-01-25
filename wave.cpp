f4e757a3bf6b55279d2422d902dd4c06dd380267
Connor Fricke <cd.fricke23@gmail.com>
2024-01-24 20:15:37 -0500

    version 1!

--- wave.cpp
+++ wave.cpp
@@ -9,7 +9,39 @@ REVISIONS
 */
 
 #include <iostream>
-#include <time.h>
+#include <cmath>
+#include <windows.h> /* for Sleep() */
+
 using namespace std;
 
-char character('+');
+// DECLARATIONS
+int height(const double x);
+
+// what the wave is made of
+char thing(':');
+
+int main()
+{
+  double iter = 0;
+
+  while (iter < 200)
+  {
+    cout << "OOO ";
+    for (int i = 0; i < height(iter); i++)
+      cout << thing;
+    cout << endl;
+
+    Sleep(5);   // sleep for 5 ms
+    iter += 0.1;
+  }
+  
+
+  return 0;
+}
+
+// height() determines the height of the wave i.e. the number of characters we print out on a given line.
+int height(const double x)
+{
+  double y = 11 + 9 * sin(x);
+  return int(y);
+}
