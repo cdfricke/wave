bc07476384a0bc567900fdcaec62a593f97de06c
Connor Fricke <138180610+cdfricke@users.noreply.github.com>
2024-01-24 19:13:41 -0500

    Create wave.cpp
    The beginning!

---
+++ wave.cpp
@@ -0,0 +1,15 @@
+/*
+Created by: Connor Fricke (cd.fricke23@gmail.com)
+File: wave.cpp
+TODO:
+  - everything
+REVISIONS
+24-Jan-2024
+  - first attempt at creating program
+*/
+
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+char character('+');
